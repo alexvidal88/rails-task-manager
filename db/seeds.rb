ccf7e1e973a14247d4db9ef5300dbdb54356f23b
Alex Vidal <alexr.vidalh@gmail.com>
2021-06-14 10:46:03 +1000

    todo list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Task.create(
+    title: Faker::TvShows::BreakingBad.character,
+    details: Faker::TvShows::BreakingBad.episode,
+    completed: Faker::Boolean.boolean
+    )
+end
